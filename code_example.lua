2d07d81f7d3aa21a878e516d05367f7e16a28496
MCPETH <thtongame@gmail.com>
2023-12-03 11:29:22 +0700

    Create code_example.lua

---
+++ code_example.lua
@@ -0,0 +1,25 @@
+
+
+local args = {
+    [1] = 1
+}
+
+game:GetService("ReplicatedStorage").Remotes.Strength:FireServer(unpack(args))
+
+
+
+
+local args = {
+    [1] = 1
+}
+
+game:GetService("ReplicatedStorage").Remotes.Agility:FireServer(unpack(args))
+
+
+local args = {
+    [1] = 1
+}
+
+game:GetService("ReplicatedStorage").Remotes.Durability:FireServer(unpack(args))
+
+
